466acbc8f4d44caaa37441d3c9f877c1531db91f
app1echip <naife@foxmail.com>
2020-12-16 16:42:25 +0800

    new: timeSeg

--- src/main/java/cn/edu/xmu/goods/model/dto/Dummy.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.xmu.goods.model.dto;
-
-import lombok.Data;
-
-@Data
-public class Dummy {
-    private String message = "remove it if you have created actual classes within this directory";
-}
--- src/main/java/cn/edu/xmu/goods/service/TimeSegmentServiceInterface.java
+++ src/main/java/cn/edu/xmu/goods/service/TimeSegmentServiceInterface.java
@@ -1,9 +1,13 @@
 package cn.edu.xmu.goods.service;
 
+import cn.edu.xmu.goods.model.dto.FlashSaleTimeSegmentDTO;
+
 import java.util.List;
 
 public interface TimeSegmentServiceInterface {
     List<Long> getCurrentFlashSaleTimeSegs();
 
     Boolean timeSegIsFlashSale(Long id);
+
+    FlashSaleTimeSegmentDTO getFlashSaleTimeSegmentById(Long id);
 }
